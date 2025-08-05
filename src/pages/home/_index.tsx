abf0c7841d6eba575e5abe639376a93a7ff54e28
mcsoud <fmcsoud@gmail.com>
2025-08-05 10:14:08 +0300

    Added notes and removed examples

--- src/pages/home/_index.tsx
+++ src/pages/home/_index.tsx
@@ -1,9 +1,5 @@
-import { Main, Modal } from "@mcsoud/react-ui";
-import useQueryFaqs from "@/hooks/queries/faqs";
-import { useLaravelQuery } from "@mcsoud/react-ui";
-import { LaravelObject, LaravelResponse } from "@mcsoud/types";
-import { useQueryClient } from "@tanstack/react-query";
-import QUERY_KEYS from "@/constants/utils/queryKeys";
+import { Main } from "@mcsoud/react-ui";
+import { LaravelObject } from "@mcsoud/types";
 
 export interface FAQ extends LaravelObject {
   question: string;
@@ -11,31 +7,10 @@ export interface FAQ extends LaravelObject {
 }
 
 export default function PageHome() {
-  const faqs = useQueryFaqs();
-  const Display = useLaravelQuery({
-    query: faqs,
-  });
-  const { data } = faqs;
-  const queryClient = useQueryClient();
-  function change() {
-    queryClient.setQueryData(QUERY_KEYS.faqs, {
-      success: true,
-      data: [{ question: "test" }],
-    });
-  }
-  const res = queryClient.getQueryData<LaravelResponse<FAQ[]>>(QUERY_KEYS.faqs);
-  const test = res?.success && res.data[0].question;
   return (
     <Main id="home">
       <h1 hidden>Home</h1>
-      <section id="home-faqs" className="container">
-        <Display success={(data) => data[0].question} />
-        <button type="button" onClick={change} className="block">
-          {data && data.success && data.data[0].question}
-        </button>
-        <p>{test}</p>
-        <Modal id="example">Testings</Modal>
-      </section>
+      <section id="home-faqs" className="container"></section>
     </Main>
   );
 }
